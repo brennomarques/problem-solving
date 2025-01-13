91e197512fd17bf23c7740965076fa056881e54a
Brenno lima <brenno.marquesb@gmail.com>
2025-01-13 15:47:28 -0300

    chore: correção do exercise 3

--- exercise-03.ts
+++ exercise-03.ts
@@ -1,20 +1,49 @@
-type FaturamentoDia = { dia: number; valor: number };
+type FaturamentoDia = { valor: number };
 
 const faturamentoMensal: FaturamentoDia[] = [
-  { dia: 1, valor: 1000 },
-  { dia: 2, valor: 1500 },
-  { dia: 3, valor: 0 },
-  { dia: 4, valor: 2000 },
-  { dia: 5, valor: 0 },
-  { dia: 6, valor: 2500 },
-  { dia: 7, valor: 1800 },
+  { valor: 1000 },
+  { valor: 1500 },
+  { valor: 0 },
+  { valor: 2000 },
+  { valor: 0 },
+  { valor: 2500 },
+  { valor: 1800 },
 ];
 
-function calcularEstatisticas(faturamento: FaturamentoDia[]) {
-  const diasComFaturamento = faturamento.filter((dia) => dia.valor > 0);
-  return diasComFaturamento;
+interface EstatisticasFaturamento {
+  menorFaturamento: number;
+  maiorFaturamento: number;
+  diasAcimaMedia: number;
+}
+
+function calcularEstatisticas(
+  faturamento: FaturamentoDia[]
+): EstatisticasFaturamento {
+  const diasFaturamento = faturamento.filter((dia) => dia.valor > 0);
+
+  const valores = diasFaturamento.map((dia) => dia.valor);
+  const soma = valores.reduce((acc, valor) => acc + valor, 0);
+  const media = soma / valores.length;
+
+  const menorFaturamento = valores.reduce(
+    (min, valor) => (valor < min ? valor : min),
+    valores[0]
+  );
+
+  const maiorFaturamento = Math.max(...valores);
+  const diasAcimaMedia = valores.filter((valor) => valor > media).length;
+
+  return {
+    menorFaturamento,
+    maiorFaturamento,
+    diasAcimaMedia,
+  };
 }
 
 const estatisticas = calcularEstatisticas(faturamentoMensal);
 
-console.log(`Faturamento: ${estatisticas}`);
+console.log(`Menor faturamento: ${estatisticas.menorFaturamento}`);
+console.log(`Maior faturamento: ${estatisticas.maiorFaturamento}`);
+console.log(
+  `Dias com faturamento acima da média: ${estatisticas.diasAcimaMedia}`
+);
